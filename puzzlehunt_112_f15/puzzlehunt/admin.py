7aace13787a1e75dba0349a36097813e32021592
Kevin Zheng <sunday0303@gmail.com>
2015-10-30 14:07:05 -0400

    Register admin pages

--- puzzlehunt_112_f15/puzzlehunt/admin.py
+++ puzzlehunt_112_f15/puzzlehunt/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
-
-# Register your models here.
-
+from .models import Team, TeamMember, Puzzle, Hint, PuzzleProgress
 
 
+class HintInline(admin.StackedInline):
+    model = Hint
+    extra = 1
+
+class PuzzleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Meta',
+            {'fields': [
+                'title',
+                'subtitle',
+                'authors',
+            ]}),
+        ('Puzzle',
+            {'fields': [
+                'flavortext',
+                'solution',
+                'time_limit',
+                'par_score',
+                'image'
+            ]})
+    ]
+    inlines = [HintInline]
+
+admin.site.register(Puzzle, PuzzleAdmin)
+
+
+class TeamMemberInline(admin.TabularInline):
+    model = TeamMember
+    extra = 4
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [TeamMemberInline]
+
+admin.site.register(Team, TeamAdmin)

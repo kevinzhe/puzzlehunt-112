ec1efbe39c91be8c050bf29d1baee43a0768aa6b
Kevin Zheng <sunday0303@gmail.com>
2015-11-06 02:45:55 -0500

    Add puzzle metadata to puzzle page

--- puzzlehunt_112_f15/puzzlehunt/views.py
+++ puzzlehunt_112_f15/puzzlehunt/views.py
@@ -9,7 +9,40 @@ def home(request):
 
 @login_required
 def puzzle(request, puzzle_id):
-    return HttpResponse(puzzle_id)
+    # Get the team
+    try: team = TeamMember.objects.get(user=request.user).team
+    except TeamMember.DoesNotExist: return redirect(home)
+    # Get the puzzle+hints
+    puzzle = get_object_or_404(Puzzle, pk=puzzle_id)
+    hints = Hint.objects.filter(puzzle=puzzle)
+    # Get the progress
+    try: progress = PuzzleProgress.objects.get(puzzle=puzzle, team=team)
+    except PuzzleProgress.DoesNotExist:
+        progress = PuzzleProgress(
+            team        = team,
+            puzzle      = puzzle,
+            start_time  = now()
+        )
+        progress.save()
+    for hint in hints:
+        hint.can_show = progress.start_time+hint.time_shown < now()
+        hint.time_to_show = int((progress.start_time+hint.time_shown).timestamp())
+    # Render the view
+    template_info = {
+        'order':        puzzle.order,
+        'title':        puzzle.title,
+        'subtitle':     puzzle.subtitle,
+        'flavortext':   puzzle.flavortext,
+        'par_score':    puzzle.par_score,
+        'start_time':   progress.start_time,
+        'end_time':     progress.end_time,
+        'completed_in': progress.completed_in, 
+        'score':        progress.score,
+        'par_duration_human': puzzle.time_limit,
+        'par_duration': int(puzzle.time_limit.total_seconds()),
+        'hints':        hints,
+    }
+    return render(request, 'puzzlehunt/puzzle.html', template_info)
 
 @login_required
 def puzzle_index(request):

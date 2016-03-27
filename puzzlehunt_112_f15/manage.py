f2560e1c4c720d0d86e95ac9a413162ea7da41bf
Kevin Zheng <sunday0303@gmail.com>
2016-03-27 13:57:23 -0500

    Update module names

--- puzzlehunt_112_f15/manage.py
+++ puzzlehunt_112_f15/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "puzzlehunt_112_f15.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "puzzlehunt_112.settings")
 
     from django.core.management import execute_from_command_line
 
